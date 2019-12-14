0908513df39d302225235924ba4004f880108237
Mohammad Ruhul Amin <mohammadruhul.amin@carsales.com.au>
2019-12-14 19:59:05 +1100

    home/showroom feature step 5: some progress but not pass yet

--- BddShop.Tests/Features/Home/Showroom/ShowroomFeatureTests.cs
+++ BddShop.Tests/Features/Home/Showroom/ShowroomFeatureTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using BddShop.Features.Home.Showroom;
 using BddShop.Tests.Infra;
 using BddShop.Tests.Infra.Fakes;
 using Bolt.RequestBus;
@@ -24,14 +25,14 @@ namespace BddShop.Tests.Features.Home.Showroom
         }
 
         [Scenario(DisplayName = "PresentShowroomViewModel")]
-        public void PresentShowroomSection(IRequestBus bus, ShowroomViewModel vm)
+        public void PresentShowroomSection(IRequestBus bus, IResponse<ShowroomViewModel> rsp)
         {
             $"Given current tenant is carsales"
                 .x(() => _scope.ServiceProvider.SetCurrentTenant("carlsales"));
             $"And I have an instance of request bus"
                 .x(() => bus = _scope.ServiceProvider.GetService<IRequestBus>());
             $"When I send request for showroom view model"
-                .x(() => vm = bus.SendAsync<ShowroomRequest,ShowroomViewModel>(new ShowroomRequest()));
+                .x(async () => rsp = await bus.SendAsync<ShowroomRequest,ShowroomViewModel>(new ShowroomRequest()));
             $"Then I should receive a showroom view model"
                 .x(() => throw new NotImplementedException());
             $"And view model should match as expected"
---
+++ BddShop/Features/Home/Showroom/ShowroomRequestHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bolt.RequestBus;
+
+namespace BddShop.Features.Home.Showroom
+{
+    public class ShowroomRequestHandler : RequestHandlerAsync<ShowroomRequest,ShowroomViewModel>
+    {
+        protected override Task<ShowroomViewModel> Handle(IExecutionContextReader context, ShowroomRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class ShowroomRequest
+    {
+
+    }
+
+    public class ShowroomViewModel
+    {
+
+    }
+}
