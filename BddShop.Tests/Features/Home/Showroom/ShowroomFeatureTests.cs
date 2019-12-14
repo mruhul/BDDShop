d5383b81af3e3e592e4fd9a40e6760d5deb1cf62
Mohammad Ruhul Amin <mohammadruhul.amin@carsales.com.au>
2019-12-14 19:14:00 +1100

    home/showroom feature step 1: define the expected behavior

---
+++ BddShop.Tests/Features/Home/Showroom/ShowroomFeatureTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xbehave;
+
+namespace BddShop.Tests.Features.Home.Showroom
+{
+    public class ShowroomFeatureTests
+    {
+        [Scenario(DisplayName = "PresentShowroomViewModel")]
+        public void PresentShowroomSection()
+        {
+            $"Given current tenant is carsales"
+                .x(() => throw new NotImplementedException());
+            $"And I have an instance of request bus"
+                .x(() => throw new NotImplementedException());
+            $"When I send request for showroom view model"
+                .x(() => throw new NotImplementedException());
+            $"Then I should receive a showroom view model"
+                .x(() => throw new NotImplementedException());
+            $"And view model should match as expected"
+                .x(() => throw new NotImplementedException());
+        }
+    }
+}
