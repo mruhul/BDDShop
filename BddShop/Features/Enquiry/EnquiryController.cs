a9d4fcd60a9c376d037960f92bbdcf1181894945
Mohammad Ruhul Amin <mohammadruhul.amin@carsales.com.au>
2020-01-08 21:56:01 +1100

    enquiry feature - success

--- BddShop/Features/Enquiry/EnquiryController.cs
+++ BddShop/Features/Enquiry/EnquiryController.cs
@@ -9,21 +9,41 @@ namespace BddShop.Features.Enquiry
     public class EnquiryController : Controller
     {
         private readonly ILeadApiProxy _proxy;
+        private readonly IProductApiProxy _productApiProxy;
+        private readonly IEmailSender _emailSender;
 
-        public EnquiryController(ILeadApiProxy proxy)
+        public EnquiryController(ILeadApiProxy proxy,IProductApiProxy productApiProxy, IEmailSender emailSender)
         {
             _proxy = proxy;
+            _productApiProxy = productApiProxy;
+            _emailSender = emailSender;
         }
 
         [HttpPost("")]
         public async Task<ActionResult> Post(SendEnquiryRequest request)
         {
+            var stock = await _productApiProxy.GetAsync(request.NetworkId);
+
             var id = new Guid().ToString();
 
             await _proxy.SendAsync(new LeadProxyInput
             {
                 NetworkId = request.NetworkId,
                 Id = id
+            });
+
+            await _emailSender.SendAsync(new SendEmailInput
+            {
+                Subject = "A enquiry posted on your item",
+                TemplateName = "EmailSellerLead",
+                To = stock.SellerEmail
+            });
+
+            await _emailSender.SendAsync(new SendEmailInput
+            {
+                TemplateName = "EnquiryThankYou",
+                Subject = "Thanks for your enquiry",
+                To = request.Email
             });
 
             return Ok(new SendEnquiryResponse
